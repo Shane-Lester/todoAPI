7e56851265dd3ef3a86002355cee1590b8b10a8b
Shane-Lester <shane_lester@hotmail.com>
2016-02-15 11:29:50 +0000

    added individual routes

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ var todos = [{
 	description: "Learn Git and be awesome",
 	completed: true,
 	id:2
+},
+{
+	description: "Deploy to Heroku",
+	completed: false,
+	id:3
 }
 ];
 
@@ -23,8 +28,23 @@ app.get('/todos', function(req,res){
 	res.json(todos);
 });
 
-app.get('/todos:id', function(req,res){
-	
+app.get('/todos/:id', function(req,res){
+	// res.send('Asking for Todo with id of ' + req.params.id);
+	var lookedforID = parseInt(req.params.id,10);
+	var foundID;
+
+	todos.forEach(function(todo){
+		if(todo.id === lookedforID){
+			foundID = todo;
+		}
+	});
+
+	if(foundID){
+		res.json(foundID);
+		}
+	else{
+		res.status(404).send();
+	}
 });
 
 
