f205ba63575ccc06f5c1db9480805bad704a1863
Shane-Lester <shane_lester@hotmail.com>
2016-02-15 11:10:02 +0000

    added a todos get request

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	description: "Learn Express",
+	completed: false,
+	id:1
+},
+{
+	description: "Learn Git and be awesome",
+	completed: true,
+	id:2
+}
+];
+
+
 
 app.get('/', function(req,res){
 	res.send('Todo API root');
 }) ;
+
+app.get('/todos', function(req,res){
+	res.json(todos);
+});
+
+app.get('/todos:id', function(req,res){
+	
+});
+
+
 
 app.listen(PORT, function(){
 	console.log('Express server listening on port ' + PORT + '!');
